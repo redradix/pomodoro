332fe97fbc8470b045429c9854719438873a1b46
Victor <victor@redradix.com>
2021-12-03 13:37:09 +0100

    Some space between arrage, act, assert

--- src/main.test.js
+++ src/main.test.js
@@ -3,22 +3,27 @@ import { createPomodoro } from './main.js'
 describe('Create Pomodoro', () => {
   it('creates pomodoro with default duration', () => {
     const pomodoro = createPomodoro()
+
     expect(pomodoro.getDuration()).toEqual(25)
   })
+
   it('creates pomodoro with custom duration', () => {
     const pomodoro = createPomodoro(10)
+
     expect(pomodoro.getDuration()).toEqual(10)
   })
 
   it('not started pomodoro returns default time', () => {
     const pomodoro = createPomodoro()
     const duration = pomodoro.getDuration()
+
     expect(pomodoro.getRemainingTime()).toEqual(duration * 60 * 1000)
   })
 
   it('starts pomodoro and decrements duration', done => {
     const pomodoro = createPomodoro()
     const remainingTime1 = pomodoro.getRemainingTime()
+
     pomodoro.start()
 
     setTimeout(() => {
